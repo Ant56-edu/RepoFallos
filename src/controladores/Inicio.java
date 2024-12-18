7a3368a500f3c0a605dee4e132f3600b1db8981e
Francisco <csi24-fdomnez@olegioaltair.net>
2024-12-18 11:57:09 +0100

    Inicializa el proyecto Java con el archivo Main.java

---
+++ src/controladores/Inicio.java
@@ -0,0 +1,13 @@
+package controladores;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo.");
+		
+		System.out.println("Adiós Mundo.");
+
+	}
+
+}
