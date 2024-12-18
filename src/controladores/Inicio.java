92d415e287629e0c759aae2037df2cf157005e49
Francisco <csi24-fdomnez@olegioaltair.net>
2024-12-18 12:10:49 +0100

    Modifica el mensaje el alumno A

--- src/controladores/Inicio.java
+++ src/controladores/Inicio.java
@@ -4,9 +4,9 @@ public class Inicio {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola Mundo.");
+		System.out.println("Hola Mundo. Adiós Mundo.");
 		
-		System.out.println("Adiós Mundo.");
+		System.out.println("Hola Sevilla.");
 
 	}
 
